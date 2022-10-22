98f352ee1c4bc9bab29695d9e21e0d8909ff0028
antoine pecheux <ant.pecheux@gmail.com>
2022-10-22 21:10:06 +0200

    home header done

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,12 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
-
+Cloudinary.config do |config|
+  config.cloud_name = 'peachux'
+  config.api_key = '355851215977576'
+  config.api_secret = 'WZ6pWQcmIDnHA_5UYzrustkNJBs'
+  config.secure = true
+end
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 
