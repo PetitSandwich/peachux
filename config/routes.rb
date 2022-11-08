099c075b1a8dcbd8fe3cc9fce9625630ad4d828b
PetitSandwich <ant.pecheux@gmail.com>
2022-11-08 17:04:19 +0100

    working on nav and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :pages
+
   authenticate :user do
 
     resources :works, only: [:index, :show, :new, :create, :edit, :update, :destroy]
@@ -9,4 +11,6 @@ Rails.application.routes.draw do
   end
   # Defines the root path route ("/")
   root "pages#home"
+  get "about", to: "pages#about"
+  # root "pages#home"
 end
